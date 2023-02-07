1812c1f864d19854fbabf8374b81aab894029af0
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2023-02-07 16:57:41 -0300

    dev

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -10,15 +10,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
-// module.exports = ({ env }) => ({
-//   "vercel-deploy": {
-//     enabled: true,
-//     config: {
-//       deployHook: process.env("VERCEL_DEPLOY_PLUGIN_HOOK"),
-//       apiToken: process.env("VERCEL_DEPLOY_PLUGIN_API_TOKEN"),
-//       appFilter: process.env("VERCEL_DEPLOY_PLUGIN_APP_FILTER"),
-//       roles: [process.env("VERCEL_DEPLOY_PLUGIN_ROLES")],
-//     },
-//   },
-// });
