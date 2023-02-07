8aec010e49912a88bd972bd4948582d30ab19d38
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2023-02-07 14:50:39 -0300

    fix: teste

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -6,7 +6,6 @@ module.exports = ({ env }) => ({
         "https://api.vercel.com/v1/integrations/deploy/prj_8GTYTvZ7eKXhIUiYkXm69XCcwGWt/Txs3471fnq",
       apiToken: "p4KPHEHaYdI2ksYW9W1r1816",
       appFilter: "e-commerce",
-      teamFilter: "",
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
   },
