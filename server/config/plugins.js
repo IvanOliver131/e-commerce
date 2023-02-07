a4a3e8299a618c65bf7215a276eca676642aee25
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2023-02-07 15:15:37 -0300

    teste 2

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -2,11 +2,10 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_8GTYTvZ7eKXhIUiYkXm69XCcwGWt/Txs3471fnq",
-      apiToken: "p4KPHEHaYdI2ksYW9W1r1816",
-      appFilter: "e-commerce",
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      deployHook: env("VERCEL_DEPLOY_PLUGIN_HOOK"),
+      apiToken: env("VERCEL_DEPLOY_PLUGIN_API_TOKEN"),
+      appFilter: env("VERCEL_DEPLOY_PLUGIN_APP_FILTER"),
+      roles: [env("VERCEL_DEPLOY_PLUGIN_ROLES")],
     },
   },
 });
