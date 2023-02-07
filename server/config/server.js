a6ffd01aa4dbaf0c54932d2f0ecf4257e80c2f2c
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2023-02-07 14:25:04 -0300

    fix:teste

--- server/config/server.js
+++ server/config/server.js
@@ -4,4 +4,15 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_8GTYTvZ7eKXhIUiYkXm69XCcwGWt/Txs3471fnq",
+      apiToken: "p4KPHEHaYdI2ksYW9W1r1816",
+      appFilter: "e-commerce",
+      teamFilter: "",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
 });
